d6404e1208a050fa21128b5fbff8bb891a3405d5
Eric Warren <eswarren18@gmail.com>
2024-12-11 10:01:48 -0700

    added post and delete salesperson endpoint

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import (
     api_salespeople,
+    api_salesperson,
 )
 
 
 urlpatterns = [
     path("salespeople/", api_salespeople, name="api_salespeople"),
+    path("salespeople/<int:id>/", api_salesperson, name="api_salesperson"),
 ]
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+import json
 
 from .models import Salesperson, Customer, Sale, AutomobileVO
 from common.json import ModelEncoder
@@ -15,9 +16,18 @@ class SalespersonListEncoder(ModelEncoder):
         "employee_id",
     ]
 
-
 @require_http_methods(["GET", "POST"])
 def api_salespeople(request):
     if request.method == "GET":
         salespeople = Salesperson.objects.all()
         return JsonResponse({"salespeople": salespeople}, encoder=SalespersonListEncoder)
+    else:
+        content = json.loads(request.body)
+        salesperson = Salesperson.objects.create(**content)
+        return JsonResponse(salesperson, encoder=SalespersonListEncoder, safe=False)
+
+@require_http_methods(["DELETE"])
+def api_salesperson(request, id):
+    if request.method == "DELETE":
+        number, _ = Salesperson.objects.filter(id=id).delete()
+        return JsonResponse({"Deleted": number > 0})
